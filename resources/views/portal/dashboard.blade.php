f5c77515b605af4558f20d322dfd403ae4baafb7
Julius Adrian <juliusa368@gmail.com>
2023-01-29 13:01:40 +0700

    add two charts

--- resources/views/portal/dashboard.blade.php
+++ resources/views/portal/dashboard.blade.php
@@ -16,7 +16,78 @@
             </form>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script type="module">
+        const ctx = document.getElementById('myCharts').getContext('2d');
+        //const labels = Utils.months({count: 7});
+        const data = {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            datasets: [
+            {
+            label: ['My First Dataset'],
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+            },
+            {
+            label: ['My Second Dataset'],
+            data: [65, 59, 85, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(255, 192, 192)',
+            tension: 0.1
+            }
+        ]
+        };
+        const config = {
+            type: 'line',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                    beginAtZero: true
+                    }
+                }
+            }
+        };
 
+        new Chart(ctx, config);
+
+        const ctx2 = document.getElementById('myCharts2').getContext('2d');
+        //const labels = Utils.months({count: 7});
+        const data2 = {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            datasets: [
+            {
+            label: ['My First Dataset'],
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+            },
+            {
+            label: ['My Second Dataset'],
+            data: [65, 59, 85, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(255, 192, 192)',
+            tension: 0.1
+            }
+        ]
+        };
+        const config2 = {
+            type: 'line',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                    beginAtZero: true
+                    }
+                }
+            }
+        };
+
+        new Chart(ctx2, config);
+    </script>
     {{-- site box --}}
     <div class="row">
         <div class="col-lg col-sm">
