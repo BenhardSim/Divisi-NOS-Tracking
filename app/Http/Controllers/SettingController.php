3770e969743bb69ab00eb0e3a1e20b973b5a7575
Julius Adrian <juliusa368@gmail.com>
2023-01-29 21:16:23 +0700

    Add SettingController

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class SettingController extends Controller
+{
+    //
+    public function index(){
+        return view('portal.setting', [
+            'title' => 'Setting'
+        ]);
+    }
+    public function update(Request $request)
+    {
+        //
+        $rules = [
+            'password' => 'required',
+            'new' => 'required',
+        ];
+
+        if(!Hash::check($request->password, auth()->user()->password)){
+            return redirect('/dashboard/setting')->with('updateError', 'Password lama salah');
+        }
+        if($request->new == $request->password){
+            return redirect('/dashboard/setting')->with('duplicateError', 'Password baru dan lama sama');
+        }
+        $validatedData =  $request->validate($rules);
+
+        $finalData = [
+            'password' => bcrypt($validatedData['new'])
+        ];
+        //$validatedData['user_id'] = auth()->user()->id;
+        //$validatedData['excerpt'] = Str::limit(strip_tags($request->body) , 50, '...');
+
+        User::where('id', auth()->user()->id)->update($finalData);
+
+        return redirect('/dashboard/setting')->with('success', 'Password berhasil diperbarui');
+    }
+}
---
+++ resources/views/portal/setting.blade.php
@@ -0,0 +1,61 @@
+@extends('portal.layouts.main')
+
+@section('container')
+
+    {{-- title section  --}}
+    <div class="mt-4 navbar d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+        {{-- <h1 class="h2 mt-4">Welcome back, {{ auth()->user()->name }}</h1> --}}
+        <div class="header-title">
+            <h3 class="" style="font-weight: normal;">NOS Portal | Setting</h3>
+        </div>
+        <div class="profile-pic">
+            <h6>{{ auth()->user()->name }}</h6>
+            <form action="/logout" method="POST">
+                @csrf
+                <button type="submit" class="btn btn-danger"><span data-feather="log-out" class="align-text-bottom"></span> Logout</button>
+            </form>
+        </div>
+    </div>
+    @if (session()->has('success'))
+        <div class="alert alert-success col-lg-10" role="alert">
+          {{ session('success') }}
+        </div>
+    @endif
+    @if (session()->has('updateError'))
+        <div class="alert alert-danger col-lg-10" role="alert">
+          {{ session('updateError') }}
+        </div>
+    @endif
+    @if (session()->has('duplicateError'))
+        <div class="alert alert-danger col-lg-10" role="alert">
+          {{ session('duplicateError') }}
+        </div>
+    @endif
+    <div class="col-lg-8">
+        <form action="/dashboard/setting" method="POST">
+        @method('put')
+        @csrf
+            <div class="mb-3">
+                <label for="password" class="form-label">Old Password</label>
+                <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" required autofocus >
+                @error('password')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div> 
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="new" class="form-label">New Password</label>
+                <input type="password" class="form-control @error('new') is-invalid @enderror" id="new" name="new" required >
+                @error('new')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div> 
+                @enderror
+            </div>
+            <div class="mb-5">
+                <button type="submit" class="btn btn-primary">Change Password</button>
+            </div>
+        </form>
+    </div>
+@endsection
