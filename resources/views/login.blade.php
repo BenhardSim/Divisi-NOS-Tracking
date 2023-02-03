8eb72ec0c1aedaea7c77c130eb864fe9756b41f6
Julius Adrian <juliusa368@gmail.com>
2023-02-03 12:57:03 +0700

    Tidying up login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -61,7 +61,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
     <link href="/css/login.css" rel="stylesheet">
     <title>Hello, world!</title>
   </head>
@@ -69,7 +70,7 @@
     <section class="container-login shadow-lg">
     <div class="row" style="height: 100vh">
 
-      <div class="col-lg-8 login-img" style="padding: 0px; margin: 0px">
+      <div class="col-lg-8 login-img rounded-left" style="padding: 0px; margin: 0px">
         
       </div>
 
@@ -86,37 +87,37 @@
           </div>
         @endif
         
-        <main class=" form-signin w-100 m-auto bg-white rounded p-5">
-            <h1 class="h3 mb-3 fw-normal text-center">Please Log In</h1>
-            <form method="POST" action="/login">
-              @csrf
-              <div class="d-flex justify-content-center">
-                <img class="mb-4" src="img/logo-telkomsel-baru.png" alt="" width="136" height="57">
-              </div>
-          
-              <div class="form-floating">
-                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
-                <label for="email">Email address</label>
-                @error('email')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div> 
-                  @enderror
-              </div>
-              <div class="form-floating">
-                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
-                <label for="password">Password</label>
-                @error('password')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div> 
+        <main class="form-signin w-100 m-auto bg-white rounded p-5">
+          <h1 class="h3 mb-3 fw-normal text-center">Please Log In</h1>
+          <form method="POST" action="/login">
+            @csrf
+            <div class="d-flex justify-content-center">
+              <img class="mb-4" src="img/logo-telkomsel-baru.png" alt="" width="136" height="57">
+            </div>
+        
+            <div class="form-floating">
+              <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+              <label for="email">Email address</label>
+              @error('email')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div> 
                 @enderror
-              </div>
-              <button class="mt-3 w-100 btn btn-lg btn-primary" type="submit">Login</button>
-              <p class="mt-2 mb-2 text-muted">&copy; 2023</p>
-            </form>
-            <small class="d-block text-center mt-1">Not registered? <a href="/register">Register Now</a></small>
-        </main>
+            </div>
+            <div class="form-floating">
+              <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" placeholder="Password" required>
+              <label for="password">Password</label>
+              @error('password')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div> 
+              @enderror
+            </div>
+            <button class="mt-3 w-100 btn btn-lg btn-primary" type="submit">Login</button>
+            <p class="mt-2 mb-2 text-muted">&copy; 2023</p>
+          </form>
+          <small class="d-block text-center mt-1">Not registered? <a href="/register">Register Now</a></small>
+      </main>
       </div>
   </div>
 </section>
@@ -125,8 +126,6 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
 </html>
