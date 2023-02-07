2398242373856bee765911514ae40f9729741a8a
Benhard Sim <36811298+BenhardSim@users.noreply.github.com>
2023-02-07 20:43:07 +0700

    Add profit loss chart NOP level

--- resources/views/portal/dashboard.blade.php
+++ resources/views/portal/dashboard.blade.php
@@ -9,30 +9,35 @@
             <h4 class="" style="font-weight: normal;">NOS Portal | Dashboard</h4>
         </div>
         <div class="profile-pic">
-            <h6>{{ auth()->user()->name }}</h6>
-            <h6>
-                @can('staff')
-                 You're on a staff level account
-                @endcan
-                @can('supervisor')
-                 You're on a supervisor level account
-                @endcan
-                @can('manager')
-                 You're on a manager level account
-                @endcan
-                @can('gm')
-                 You're on a Grand Master level account
-                @endcan
-                @can('admin')
-                 You're on an admin level account
-                @endcan
-            </h6>
-            <form action="/logout" method="POST">
-                @csrf
-                <div class="btn-wrao">
-                    <button style="vertical-align: middle;" type="submit" class="btn btn-danger btn-sm"><span data-feather="log-out" class="align-text-bottom"></span> Logout</button>
-                </div>
-            </form>
+            <div style="padding-right: 10px">
+                <p>
+                    <b>{{ auth()->user()->name }}</b>
+                    <br>
+                    @can('staff')
+                     You're on a staff level account
+                    @endcan
+                    @can('supervisor')
+                     You're on a supervisor level account
+                    @endcan
+                    @can('manager')
+                     You're on a manager level account
+                    @endcan
+                    @can('gm')
+                     You're on a Grand Master level account
+                    @endcan
+                    @can('admin')
+                     You're on an admin level account
+                    @endcan
+                </p>
+            </div>
+           <div>
+               <form action="/logout" method="POST">
+                   @csrf
+                   <div class="btn-wrao">
+                       <button style="vertical-align: middle;" type="submit" class="btn btn-danger"><span data-feather="log-out" class="align-text-bottom"></span> Logout</button>
+                   </div>
+               </form>
+           </div>
         </div>
     </div>
     {{-- site box --}}
