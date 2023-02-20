e6ee51d5250ca3cdacade339003377542146a2f9
Benhard Sim <36811298+BenhardSim@users.noreply.github.com>
2023-02-20 22:54:13 +0700

    fix upload-document design

--- resources/views/portal/upDokumen.blade.php
+++ resources/views/portal/upDokumen.blade.php
@@ -13,6 +13,7 @@
 {{-- upload file  --}}
 <div>
   <div class="row">
+
     <div class="col-7 shadow up-docs-sign">
         <h6>Document Approval</h6>
         <form method="POST" action="/tracked_document" enctype="multipart/form-data">
@@ -101,7 +102,6 @@
         </form>
       </div>
 
-      
       <div class="col-4 up-docs-sign shadow">
         <h6>History Pengiriman</h6>
         <div>
@@ -125,7 +125,7 @@
                 
               <tr>
                 <td>
-                  <a href="tracked_document/{{ $document->id }}" class="links text-dark">
+                  {{-- <a href="tracked_document/{{ $document->id }}" class="links text-dark">
                     <p style="padding: 0%">
                       <b>Pengirim</b> : {{ $document->nama_pengirim }} <br>
                       <b>Nama Dokumen</b> : {{ $document->file }} <br>
@@ -139,7 +139,21 @@
                       <b>Approval Level 3</b> :@isset($history3) {{ $history3->waktu->format('D, d M Y H:i')  }} @else - @endisset<br>
                       <b>Approval Level 4</b> :@isset($history4) {{ $history4->waktu->format('D, d M Y H:i')  }} @else - @endisset<br>
                     </p>  
-                  </a>
+                  </a> --}}
+  
+                  <a href="tracked_document/{{ $document->id }}" class="links text-dark">
+                    <div class="row">
+                      <div id='initial' class="rounded-circle shadow-sm col-2" style="background-color: #eb3223;color:white;width:30px;height:30px;display:flex;justify-content:center;align-items:center;vertical-align:middle">
+                          {{ $document->file[0] }}{{ $document->file[1] }}
+                      </div>
+                      <div class="col-8">
+                        <h6 style="margin:0">{{ $document->file }}</h6>
+                        <p style="color:#3F979B;margin:0">pengirim : {{ $document->nama_pengirim }}</p>
+                        <p style="margin:0;width: 30ch;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;">{{ $document->deskripsi }}</p>
+                      </div>
+                      <div class="col-2" style="padding:0"><span style="font-size: 10px;padding:0">{{ $document->tanggal->format('d M Y') }}</span></div>
+                    </div>
+                  </a> 
                 </td>
               </tr> 
             
@@ -150,7 +164,11 @@
       </div>
     </div>
 
-    <h2 class="mt-4 mb-3">Log History</h2>
+</div>
+    
+<br><br><br>  
+
+    {{-- <h2 class="mt-4 mb-3">Log History</h2>
     <div class="list-group col-lg-8 shadow">
       @foreach ($histories as $history)
       @php
@@ -174,10 +192,15 @@
           <small>{{ $user->name }}</small>
         </div>
       @endforeach   
-  </div>
-    
-</div>
+  </div> --}}
+
+  
+
 
 
 
 @endsection
+
+
+
+
